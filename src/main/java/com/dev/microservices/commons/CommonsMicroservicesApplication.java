5d8163e11595cc7d2b8fa14f0edac62fbc705cc1
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 16:55:35 -0500

    starting project

---
+++ src/main/java/com/dev/microservices/commons/CommonsMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.microservices.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsMicroservicesApplication.class, args);
+	}
+
+}
